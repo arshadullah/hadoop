abc46f92e8adbda30ee03b84cbdb4ecead5f80ce
arshadullah <arshadak9270@gmail.com>
2019-12-10 23:05:39 +0400

    Update install_tuned.sh

--- install_tuned.sh
+++ install_tuned.sh
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Copyright Clairvoyant 2016
+# Copyright 
 
 # Function to discover basic OS details.
 discover_os() {
