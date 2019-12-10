ee8c73ee030208766164a6ccfc941cc718b3d93f
arshadullah <arshadak9270@gmail.com>
2019-12-10 23:06:54 +0400

    Update install_ntp.sh

--- install_ntp.sh
+++ install_ntp.sh
@@ -12,8 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Copyright Clairvoyant 2015
-
+# Copyright 
 DATE=$(date +'%Y%m%d%H%M%S')
 
 # Function to discover basic OS details.
